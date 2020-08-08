8ac049891db671d6ead29208e2948dd22955d704
Sébastien d'Olbekce <sebastien@mies.co>
2020-08-08 17:51:34 +0200

    Improve archive dates

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import { withSecrets } from "@mies-co/next-secrets";
 import Link from "next/link";
+import styled from "styled-components";
 
 import Layout from "_comps/Layout";
+
+const DateLast = styled.div`
+	font-size: 80%;
+	color: ${theme.gray};
+`;
 
 function Home(props) {
 	const { posts } = props;
@@ -9,7 +15,14 @@ function Home(props) {
 		<Layout>
 			{posts.map(
 				({
-					frontmatter: { title, description, dateCreated, dateUpdated, dateLast },
+					frontmatter: {
+						title,
+						description,
+						dateCreated,
+						dateUpdated,
+						dateLast,
+						dateLastHuman,
+					},
 					slug,
 				}) => (
 					<article key={slug}>
@@ -21,7 +34,7 @@ function Home(props) {
 									</a>
 								</Link>
 							</h3>
-							{dateLast && <span>Date: {dateLast}</span>}
+							<DateLast>{dateLastHuman}</DateLast>
 						</header>
 						<section>
 							<p>{description}</p>
