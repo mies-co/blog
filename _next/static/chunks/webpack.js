6bf66db976ac635512ba573e0b269abefab92784
Sébastien d'Olbecke <s.dolbecke@gmail.com>
2020-12-17 16:33:33 +0100

    Deploy Next.js to gh-pages

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cc75dee8301c0b5815cb";
+/******/ 	var hotCurrentHash = "401a74b32925fbeebd03";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
